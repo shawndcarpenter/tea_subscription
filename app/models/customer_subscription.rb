e0c9383845698f44ad4dab861628cca85f31118a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-25 12:14:13 -0700

    Add error handling poro and serializer

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,4 +1,10 @@
 class CustomerSubscription < ApplicationRecord
   belongs_to :customer
   belongs_to :subscription
+
+  validates_presence_of :customer_id
+  validates_uniqueness_of :customer_id
+
+  validates_presence_of :subscription_id
+  validates_uniqueness_of :subscription_id
 end
