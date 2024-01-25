25b61f7e5bbbfe5bebada1503bf545169fd7b769
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-25 12:13:16 -0700

    Add sad path testing for customer subscription sign up

--- spec/requests/api/v0/create_customer_subscription_request_spec.rb
+++ spec/requests/api/v0/create_customer_subscription_request_spec.rb
@@ -21,4 +21,104 @@ describe "Sign Up Customer for Tea Subscription API Endpoint" do
     expect(response_data[:message]).to be_a String
     expect(response_data[:message]).to eq("Successfully signed up Customer ##{customer.id} for #{subscription.title}")
   end 
+
+  describe "sad paths" do
+    it "gracefully handles customer does not exist" do
+      create_list(:subscription, 3)
+      subscription = Subscription.first
+
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      post '/api/v0/customer_subscriptions', params: {subscription_id: subscription.id, customer_id: 123123123 }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Customer with 'id'=123123123")
+    end
+
+    it "gracefully handles subscription does not exist" do
+      create_list(:customer, 3)
+      customer = Customer.first
+
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      post '/api/v0/customer_subscriptions', params: {subscription_id: 123123123, customer_id: customer.id }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Subscription with 'id'=123123123")
+    end
+
+    it "gracefully handles missing parameters" do
+      create_list(:customer, 3)
+      customer = Customer.first
+
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      post '/api/v0/customer_subscriptions', params: {customer_id: customer.id }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Subscription without an ID")
+    end
+
+    it "gracefully handles no parameters" do
+      create_list(:customer, 3)
+      customer = Customer.first
+
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      post '/api/v0/customer_subscriptions'
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(CustomerSubscription.all.length).to eq(0)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Customer without an ID")
+    end
+
+    it "gracefully handles customer already subscribed" do
+      create_list(:customer, 3)
+      create_list(:subscription, 3)
+  
+      subscription = Subscription.first
+      customer = Customer.first
+  
+      expect(CustomerSubscription.all.length).to eq(0)
+  
+      post '/api/v0/customer_subscriptions', params: {subscription_id: subscription.id, customer_id: customer.id }
+  
+      expect(response).to be_successful
+      expect(CustomerSubscription.all.length).to eq(1)
+
+      post '/api/v0/customer_subscriptions', params: {subscription_id: subscription.id, customer_id: customer.id }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(CustomerSubscription.all.length).to eq(1)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Customer ##{customer.id} is already signed up for #{subscription.title}")
+    end
+  end
 end
