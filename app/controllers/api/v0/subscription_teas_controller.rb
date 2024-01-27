d411ce9cb92d854589287389153486ad3e2edc6a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-27 12:37:49 -0700

    Add create method and routing for subscription teas

---
+++ app/controllers/api/v0/subscription_teas_controller.rb
@@ -0,0 +1,31 @@
+class Api::V0::SubscriptionTeasController < ApplicationController
+
+  def create
+    tea = Tea.find(params[:tea_id])
+    subscription = Subscription.find(params[:subscription_id])
+
+    if SubscriptionTea.where("tea_id = #{tea.id} and subscription_id = #{subscription.id}") != []
+      subscription_tea_exists_response(tea.title, subscription.title)
+    else
+      subscription_tea = SubscriptionTea.new(tea_id: tea.id, subscription_id: subscription.id)
+      if subscription_tea.save
+        success_response(tea.title, subscription.title)
+      end
+    end
+  end
+
+  private
+
+  def subscription_tea_exists_response(tea_title, subscription_title)
+    render json: ErrorSerializer.new(
+      ErrorMessage.new(
+        "#{tea_title} is already part of #{subscription_title}", 422
+      )).serialize_json, status: 422
+  end
+
+  def success_response(tea_title, subscription_title)
+    render json:  {
+      "message": "Successfully added #{tea_title} tea to #{subscription_title} subscription"
+    }, status: 201
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
       resources :subscriptions, only: [:create, :update]
 
+      resources :subscription_teas, only: [:create]
+
       resources :customers, only: [:create, :update] do
         resources :subscriptions, only: [:index], :controller => "customer_subscriptions"
       end
